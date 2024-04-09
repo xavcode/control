e81bc536cf8dff54e8e754348cecb4f06c277b70
Javier Diaz <javiermauriciodiz@gmail.com>
2024-04-08 22:51:02 -0500

    Add facturacion module and update GUI layout in main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tkinter import *
 from tkinter import ttk
 
 
-import guias, remesas, anexos, destinos, config
+import guias, remesas, anexos, destinos, facturacion, config
 
 # Crear la ventana principal
 window = tk.Tk()
@@ -37,29 +37,29 @@ frame_botones = ttk.Frame(window)
 frame_botones.grid(row=0,column=0, sticky="nswe", padx=10, pady=20)
 frame_botones.config()
 
-# Botón para mostrar el boton home
+
 btn_home = ttk.Button(frame_botones, text="Inicio", width=12, command=lambda: main_frame_show())
 btn_home.grid(row=0, column=0, pady=10)
 
-# Botón para mostrar el formulario de guías
+
 btn_guias = ttk.Button(frame_botones, text="Guías", command=lambda: guias.show_guias(main_frame), width=12)
 btn_guias.grid(row=1, column=0, pady=10)
 
-# Botón para mostrar el formulario de remesas
+
 btn_remesas = ttk.Button(frame_botones, text="Remesas", command=lambda: remesas.show_remesas(main_frame), width=12,)
 btn_remesas.grid(row=2, column=0, pady=10)
 
-# Botón para mostrar el formulario de anexos
+
 btn_anexos = ttk.Button(frame_botones, text="Anexos", width=12, command=lambda: anexos.show_anexos(main_frame))
 btn_anexos.grid(row=3, column=0, pady=10)
-# Botón para mostrar el formulario de anexos
+
+
+btn_facturacion = ttk.Button(frame_botones, text="Facturación", width=12, command=lambda: facturacion.show_facturacion(main_frame))
+btn_facturacion.grid(row=4, column=0, pady=10)
+
 
 btn_destinos = ttk.Button(frame_botones, text="Destinos", width=12, command=lambda: destinos.show_destinos(main_frame))
-btn_destinos.grid(row=4, column=0, pady=10)
-
-
-btn_facturacion = ttk.Button(frame_botones, text="Facturación", width=12, state="disabled") 
-btn_facturacion.grid(row=5, column=0, pady=10)
+btn_destinos.grid(row=5, column=0, pady=10)
 
 btn_config = ttk.Button(frame_botones, text='Configuración', width=12, command=lambda: config.show_config(main_frame))
 btn_config.grid(row=6, column=0, pady=10)
